667c0333613174ef63dbfb1e9af15f6549f2faff
adri-12 <adrian_cosma97@yahoo.ro>
2023-06-06 21:23:36 +0300

    I hid the api key using .env

--- src/api.jsx
+++ src/api.jsx
@@ -1,8 +1,5 @@
 //Base URL
 const base_url = "https://api.rawg.io/api/";
-
-//API KEY
-const KEY = "5d6a650d2df743c0a2236b13427a0a51";
 //Getting the date
 const getCurrentMonth = () => {
   const month = new Date().getMonth() + 1;
@@ -31,9 +28,15 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 //Popular Games
-const popular_games = `games?key=${KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games = `games?key=${KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const newGames = `games?key=${KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popular_games = `games?key=${
+  import.meta.env.VITE_API_KEY
+}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=${
+  import.meta.env.VITE_API_KEY
+}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?key=${
+  import.meta.env.VITE_API_KEY
+}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
@@ -41,11 +44,13 @@ export const newGamesURL = () => `${base_url}${newGames}`;
 
 //Game Details
 export const gameDetailsURL = (game_id) =>
-  `${base_url}games/${game_id}?key=${KEY}`;
+  `${base_url}games/${game_id}?key=${import.meta.env.VITE_API_KEY}`;
 //Games ScreenShots
 export const gameScreenshotURL = (game_id) =>
-  `${base_url}games/${game_id}/screenshots?key=${KEY}`;
+  `${base_url}games/${game_id}/screenshots?key=${import.meta.env.VITE_API_KEY}`;
 
 //Searcher Game
 export const searchGameURL = (game_name) =>
-  `${base_url}games?search=${game_name}&page_size=9&key=${KEY}`;
+  `${base_url}games?search=${game_name}&page_size=9&key=${
+    import.meta.env.VITE_API_KEY
+  }`;
